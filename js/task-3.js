1e08864c3b3990a1c090963a4bf5da37eed2da7a
Dima_Ostapchuk <Adrenalinbd@gmail.com>
2024-06-10 21:14:39 +0300

    test

--- js/task-3.js
+++ js/task-3.js
@@ -13,3 +13,13 @@ console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
 console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
 console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
 console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+
+let arr = [];
+
+console.log(fillArray(2, 10));
+
+function fillArray(min, max) {
+  for (let i = min; i <= max; i += 2) {
+    return arr.push(i);
+  }
+}
